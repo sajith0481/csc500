e341ce4c6677d5b79732077070f6147b20160100
SAJITH JOSEPH <sajith0481@gmail.com>
2024-04-20 10:46:56 +0200

    assignment_1

--- assignment1/addition_subtraction.py
+++ assignment1/addition_subtraction.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+# Prompt the user to input two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+# Add the two numbers
 addition = num1 + num2
+
+# Subtract the second number from the first number
 subtraction = num1 - num2
 
+# Print the results
 print("Addition:", addition)
 print("Subtraction:", subtraction)
 
