7b4b68cde3c525d20242de91f6684eda55f70dac
SAJITH JOSEPH <sajith0481@gmail.com>
2024-04-20 10:32:24 +0200

    assignment_1

---
+++ assignment1/addition_subtraction.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+
---
+++ assignment1/multiplication_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Prompt the user to input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Multiply the two numbers
+product = num1 * num2
+
+# Divide the first number by the second number
+quotient = num1 / num2
+
+# Print the results
+print("The product of", num1, "and", num2, "is", product)
+print("The quotient of", num1, "divided by", num2, "is", quotient)
