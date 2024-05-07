2b30a6bb7ca4d5f138aaabc9109e1305fc6830d8
SAJITH JOSEPH <sajith0481@gmail.com>
2024-05-07 22:35:53 +0200

    discussion_4

---
+++ discussion4/for_loop.py
@@ -0,0 +1,92 @@
+# s = 'Simplilearn'
+# for i in s:
+#     print(i)
+# for i in s:
+#     print(i, end='*')
+
+# Use for loop in a list
+# programming = ['Python', 'Java', 'C++', 'C#', 'Ruby']
+# for iter in programming:
+#     print(iter)
+
+# Find the avg of a list of numbers
+# list_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# sum = 0
+# for i in list_num:
+#     sum += i
+# print('The sum of the list is:', sum)
+# print('The average of the list is:', sum/len(list_num))
+
+# Use for loop in a tuple
+# num = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# sum = 0
+# for i in num:
+#     sum += i
+# print('The sum of the tuple is:', sum)
+
+# range function
+# for i in range(1, 10):
+#     print(i)
+# for i in range(0, 10, 2):
+#     print("Display values with a step size of 2:", i)
+
+# Program to print table of a given number
+# n = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     mul = n * i
+#     print(n, 'x', i, '=', mul)
+
+# list2 = ['Python', 'Java', 'C++', 'C#', 'Ruby']
+# for i in range(len(list2)):
+#     print("Hello", list2[i])
+
+# Nested for loop
+# companies = ['Google', 'Microsoft', 'Facebook']
+# for i in companies:
+#     print("We will display each letter of "+i)
+#     for letter in i:
+#         print(letter)
+
+# for loop with else clause
+# for i in range(0, 10, 3):
+#     print(i)
+# else:
+#     print("The for loop is completed")
+
+# Break statement
+# for i in range(1, 10):
+#     if i == 6:
+#         break
+#     print(i)
+
+# Continue statement
+# for i in range(1, 10):
+#     if i == 6:
+#         continue
+#     print(i)
+
+# Program to display the total goals a player has scored
+# player_name = "Gary"
+# goals = {"Edison": 14, "Bernat":3, "Ronaldo": 25, "Messi": 20}
+
+# for player in goals:
+#     if player == player_name:
+#         print(player_name, "has scored", goals[player], "goals")
+#         break
+# else:
+#     print("No player found with the name", player_name)
+
+# Cube of number
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# cube = []
+# for i in num:
+#     cube.append(i**3)
+# print(cube)
+
+# Pattern printing
+# n = int(input("Enter the number of rows: "))
+# for i in range(0,n):
+#     for j in range(0, i+1):
+#         print("*", end="")
+#     print()
+
---
+++ discussion4/while_loop.py
@@ -0,0 +1,20 @@
+# i = 0
+# while i < 10:
+#     i += 1
+#     print(i, end='# ')
+#     print()
+
+# count = 0
+# while count < 5:
+#     print("The condition is true")
+#     count = count + 1
+# print("End of Loop")
+
+# while True:
+#     print("Please type your name.")
+#     name = input()
+#     if name == 'Joe':
+#         break
+# print("Thank you! You typed the correct name.")
+
+ 
