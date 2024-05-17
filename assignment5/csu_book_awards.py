a653ea5f630c86e43697b787936b69cbe5eb34a1
Sajith Joseph <sajith0481@gmail.com>
2024-05-17 23:41:05 +0200

    assignment_5

--- assignment5/csu_book_awards.py
+++ assignment5/csu_book_awards.py
@@ -2,13 +2,13 @@
 num_books = int(input("Enter the number of books purchased this month: "))
 
 # Calculate the points based on the number of books purchased
-if num_books == 0:
+if 0 <= num_books <= 1:
     points = 0
-elif num_books == 2:
+elif 2 <= num_books <= 3:
     points = 5
-elif num_books == 4:
+elif 4 <= num_books <= 5:
     points = 15
-elif num_books == 6:
+elif 6 <= num_books <= 7:
     points = 30
 elif num_books >= 8:
     points = 60
