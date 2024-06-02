f8184c6149ba2489098533dddce4d8008da27709
SAJITH JOSEPH <sajith0481@gmail.com>
2024-06-02 14:33:17 +0200

    module8

---
+++ module8/class_example12.py
@@ -0,0 +1,21 @@
+class Employee:
+    def __init__(self, name, wage=8.25, hours=20):
+        """
+        Default employee is part time (20 hours/week)
+        and earns minimum wage
+        """
+        self.name = name
+        self.wage = wage
+        self.hours = hours
+
+    # ...
+
+
+todd = Employee('Todd')  # Typical part-time employee
+jason = Employee('Jason')  # Typical part-time employee
+tricia = Employee('Tricia', wage=12.50, hours=40)  # Manager employee
+
+employees = [todd, jason, tricia]
+
+for e in employees:
+    print ('{} earns {:.2f} per week'.format(e.name, e.wage*e.hours))
