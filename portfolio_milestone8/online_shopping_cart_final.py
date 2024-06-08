ee4fa6de8f76b382b24e96e75109b74bed1e6970
Sajith Joseph <sajith0481@gmail.com>
2024-06-08 14:01:43 +0200

    portfolio_milestone8

--- portfolio_milestone8/online_shopping_cart_final.py
+++ portfolio_milestone8/online_shopping_cart_final.py
@@ -205,8 +205,16 @@ def main():
     """
     customer_name = input("Enter customer's name: ")
     current_date = input("Enter today's date: ")
+    # Output the entered name and date
+    print(f"Customer name: {customer_name}")
+    print(f"Today's date: {current_date}")
+    # Create a shopping cart object
     cart = ShoppingCart(customer_name, current_date)
+    # Print the menu
     print_menu(cart)
 
 if __name__ == "__main__":
     main()
+
+
+
